d673656e6e6d94020e3ee435fa6bb29fb8c7acaf
eramit <eramitahirwar70@gmail.com>
2023-09-24 11:26:55 +0530

    master

---
+++ App.java
@@ -0,0 +1,30 @@
+public class App
+{
+	private int uid;
+	private String name;
+	private String password;
+	
+	public App(int arg1,String arg2,String arg3)
+	{
+		uid=arg1;
+		name=arg2;
+		password=arg3;
+	}
+	public App(App arg)
+	{
+		this.uid=arg.uid;
+		this.name=arg.name;
+		this.password=arg.password;
+	}
+	public void show()
+	{
+		System.out.println("UID:"+ uid);
+		System.out.println("username:"+ name);
+		System.out.println("password:"+ password);
+	}
+	public static void main(String[] args)
+	{
+		App obj=new App(707,"akash","cetpa");
+		obj.show();
+	}
+}
---
+++ App1.java
@@ -0,0 +1,10 @@
+public class App1
+{
+	public static void main(String[] args)
+	{
+		App abc=new App(808,"satyam","ducat");
+		App xyz=new App(abc);
+		xyz.show();
+
+	}
+}
